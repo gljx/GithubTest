abaf60773f6f1398955ca11128931087dcb68f73
高山刘水 <gljx@live.com>
2019-05-22 15:02:35 +0800

    版本回滚测试

--- src/tech/acgn/Main.java
+++ src/tech/acgn/Main.java
@@ -1,21 +1,9 @@
 package tech.acgn;
 
-import tech.acgn.InterFaces.MyInterFace;
+public class Main {
 
-public class Main implements MyInterFace {
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world");
-    }
-
-    @Override
-    public void sayhello() {
-        System.out.println("hello");
-    }
-
-    @Override
-    public void saygoodbye() {
-        System.out.println("good bye");
     }
 }
