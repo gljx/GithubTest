bf5b25e3a6caf72a357fd7a42e10d7f3443d6322
高山刘水 <gljx@live.com>
2019-05-22 14:33:10 +0800

    Implements

---
+++ src/tech/acgn/InterFaces/MyInterFace.java
@@ -0,0 +1,6 @@
+package tech.acgn.InterFaces;
+
+public interface MyInterFace {
+    void sayhello();
+    void saygoodbye();
+}
--- src/tech/acgn/Main.java
+++ src/tech/acgn/Main.java
@@ -1,9 +1,21 @@
 package tech.acgn;
 
-public class Main {
+import tech.acgn.InterFaces.MyInterFace;
 
-    public static void main(String[] args) {
+public class Main implements MyInterFace {
+
+    public static void main(String[] args){
 	// write your code here
         System.out.println("hello world");
+    }
+
+    @Override
+    public void sayhello() {
+        System.out.println("hello");
+    }
+
+    @Override
+    public void saygoodbye() {
+        System.out.println("good bye");
     }
 }
